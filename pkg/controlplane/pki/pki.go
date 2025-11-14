8aecf3c02adb7f48247f927ad262330e89f89ef1
Patryk Rostkowski <patrostkowski@gmail.com>
2025-11-15 00:14:05 +0800

    chore: create all certs in beautiful way
    Signed-off-by: Patryk Rostkowski <patrostkowski@gmail.com>

--- pkg/controlplane/pki/pki.go
+++ pkg/controlplane/pki/pki.go
@@ -15,73 +15,375 @@
 package pki
 
 import (
+	"time"
+
 	certmanagerv1 "github.com/cert-manager/cert-manager/pkg/apis/certmanager/v1"
-	v1 "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
+	certmanagermeta "github.com/cert-manager/cert-manager/pkg/apis/meta/v1"
 	mcpv1alpha1 "github.com/patrostkowski/operator-template/pkg/apis/controlplane.patrostkowski.dev/v1alpha1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
-// Resources returns all cert-manager objects that should exist for this ManagedPKI.
-// The reconciler will loop over this and CreateOrUpdate each one.
-func Resources(pki *mcpv1alpha1.ManagedPKI) []client.Object {
-	ns := pki.Namespace
-	// For now, keep it simple and just reuse the static names.
-	return []client.Object{
-		selfSignedIssuer(ns),
-		caCertificate(ns),
-		caIssuer(ns),
-	}
+type PKIIssuerSpec struct {
+	Name      string
+	Namespace string
+
+	SelfSigned bool
+	CASecret   string
 }
 
-func selfSignedIssuer(namespace string) client.Object {
-	return &certmanagerv1.Issuer{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "selfsigned",
-			Namespace: namespace,
+type PKICertificateSpec struct {
+	Name      string
+	Namespace string
+
+	SecretName string
+	CommonName string
+	IsCA       bool
+
+	IssuerName string
+
+	Duration      *metav1.Duration
+	RenewBefore   *metav1.Duration
+	Usages        []certmanagerv1.KeyUsage
+	DNSNames      []string
+	IPAddresses   []string
+	Organizations []string
+}
+
+// Resources returns all cert-manager objects that should exist for this ManagedPKI.
+func Resources(pkiObj *mcpv1alpha1.ManagedPKI) []client.Object {
+	ns := pkiObj.Namespace
+
+	tenYears := metav1.Duration{Duration: 87600 * time.Hour} // 10 years
+	thirtyDays := metav1.Duration{Duration: 720 * time.Hour} // 30 days
+
+	var objs []client.Object
+
+	// ========= Issuers =========
+	issuerSpecs := []PKIIssuerSpec{
+		// Root CA
+		{Name: "selfsigned", Namespace: ns, SelfSigned: true},
+		{Name: "ca-issuer", Namespace: ns, CASecret: "managed-ca"},
+
+		// Etcd CA
+		{Name: "etcd-selfsigned", Namespace: ns, SelfSigned: true},
+		{Name: "etcd-ca-issuer", Namespace: ns, CASecret: "etcd-ca"},
+
+		// Front-proxy CA
+		{Name: "front-proxy-selfsigned", Namespace: ns, SelfSigned: true},
+		{Name: "front-proxy-ca-issuer", Namespace: ns, CASecret: "front-proxy-ca"},
+	}
+
+	for _, is := range issuerSpecs {
+		objs = append(objs, BuildIssuer(is))
+	}
+
+	// ========= Certificates =========
+	certSpecs := []PKICertificateSpec{
+		// Root CA
+		{
+			Name:       "managed-ca",
+			Namespace:  ns,
+			SecretName: "managed-ca",
+			CommonName: "managed-ca",
+			IsCA:       true,
+			IssuerName: "selfsigned",
 		},
-		Spec: certmanagerv1.IssuerSpec{
+
+		// Etcd CA
+		{
+			Name:       "etcd-ca",
+			Namespace:  ns,
+			SecretName: "etcd-ca",
+			CommonName: "etcd-ca",
+			IsCA:       true,
+			IssuerName: "etcd-selfsigned",
+		},
+
+		// Front-proxy CA
+		{
+			Name:       "front-proxy-ca",
+			Namespace:  ns,
+			SecretName: "front-proxy-ca",
+			CommonName: "kubernetes-front-proxy-ca",
+			IsCA:       true,
+			IssuerName: "front-proxy-selfsigned",
+		},
+
+		// =========================
+		// SERVICE ACCOUNT SIGNER (uses ca-issuer)
+		// =========================
+		{
+			Name:        "sa-signer",
+			Namespace:   ns,
+			SecretName:  "sa-signer",
+			CommonName:  "sa-signer",
+			IssuerName:  "ca-issuer",
+			Duration:    &tenYears,
+			RenewBefore: &thirtyDays,
+			Usages: []certmanagerv1.KeyUsage{
+				certmanagerv1.UsageDigitalSignature,
+				certmanagerv1.UsageKeyEncipherment,
+			},
+		},
+
+		// =========================
+		// KUBERNETES LEAF CERTS (signed by ca-issuer)
+		// =========================
+
+		// kube-apiserver serving cert
+		{
+			Name:        "apiserver-tls",
+			Namespace:   ns,
+			SecretName:  "apiserver-tls",
+			CommonName:  "kube-apiserver",
+			IssuerName:  "ca-issuer",
+			Duration:    &tenYears,
+			RenewBefore: &thirtyDays,
+			Usages: []certmanagerv1.KeyUsage{
+				certmanagerv1.UsageDigitalSignature,
+				certmanagerv1.UsageKeyEncipherment,
+				certmanagerv1.UsageServerAuth,
+			},
+			DNSNames: []string{
+				"kube-apiserver." + ns + ".svc",
+				"kube-apiserver." + ns + ".svc.cluster.local",
+				"kubernetes",
+				"kubernetes.default",
+				"kubernetes.default.svc",
+				"kubernetes.default.svc.cluster.local",
+				"localhost",
+			},
+			IPAddresses: []string{"127.0.0.1"},
+		},
+
+		// kube-apiserver -> kubelet client cert (O=system:masters)
+		{
+			Name:        "apiserver-kubelet-client",
+			Namespace:   ns,
+			SecretName:  "apiserver-kubelet-client",
+			CommonName:  "kube-apiserver-kubelet-client",
+			IssuerName:  "ca-issuer",
+			Duration:    &tenYears,
+			RenewBefore: &thirtyDays,
+			Usages: []certmanagerv1.KeyUsage{
+				certmanagerv1.UsageDigitalSignature,
+				certmanagerv1.UsageKeyEncipherment,
+				certmanagerv1.UsageClientAuth,
+			},
+			Organizations: []string{"system:masters"},
+		},
+
+		// =========================
+		// ETCD LEAF CERTS (signed by etcd-ca-issuer)
+		// =========================
+
+		// etcd server
+		{
+			Name:        "etcd-server",
+			Namespace:   ns,
+			SecretName:  "etcd-server-tls",
+			CommonName:  "etcd-0.etcd." + ns + ".svc",
+			IssuerName:  "etcd-ca-issuer",
+			Duration:    &tenYears,
+			RenewBefore: &thirtyDays,
+			Usages: []certmanagerv1.KeyUsage{
+				certmanagerv1.UsageDigitalSignature,
+				certmanagerv1.UsageKeyEncipherment,
+				certmanagerv1.UsageServerAuth,
+				certmanagerv1.UsageClientAuth,
+			},
+			DNSNames: []string{
+				"etcd-0.etcd." + ns + ".svc",
+				"etcd." + ns + ".svc",
+				"localhost",
+			},
+			IPAddresses: []string{"127.0.0.1"},
+		},
+
+		// etcd peer
+		{
+			Name:        "etcd-peer",
+			Namespace:   ns,
+			SecretName:  "etcd-peer-tls",
+			CommonName:  "etcd-0.etcd." + ns + ".svc",
+			IssuerName:  "etcd-ca-issuer",
+			Duration:    &tenYears,
+			RenewBefore: &thirtyDays,
+			Usages: []certmanagerv1.KeyUsage{
+				certmanagerv1.UsageDigitalSignature,
+				certmanagerv1.UsageKeyEncipherment,
+				certmanagerv1.UsageServerAuth,
+				certmanagerv1.UsageClientAuth,
+			},
+			DNSNames: []string{
+				"etcd-0.etcd." + ns + ".svc",
+				"localhost",
+			},
+			IPAddresses: []string{"127.0.0.1"},
+		},
+
+		// etcd healthcheck client
+		{
+			Name:        "etcd-healthcheck-client",
+			Namespace:   ns,
+			SecretName:  "etcd-healthcheck-client",
+			CommonName:  "kube-etcd-healthcheck-client",
+			IssuerName:  "etcd-ca-issuer",
+			Duration:    &tenYears,
+			RenewBefore: &thirtyDays,
+			Usages: []certmanagerv1.KeyUsage{
+				certmanagerv1.UsageDigitalSignature,
+				certmanagerv1.UsageKeyEncipherment,
+				certmanagerv1.UsageClientAuth,
+			},
+		},
+
+		// apiserver -> etcd client
+		{
+			Name:        "apiserver-etcd-client",
+			Namespace:   ns,
+			SecretName:  "apiserver-etcd-client",
+			CommonName:  "kube-apiserver-etcd-client",
+			IssuerName:  "etcd-ca-issuer",
+			Duration:    &tenYears,
+			RenewBefore: &thirtyDays,
+			Usages: []certmanagerv1.KeyUsage{
+				certmanagerv1.UsageDigitalSignature,
+				certmanagerv1.UsageKeyEncipherment,
+				certmanagerv1.UsageClientAuth,
+			},
+		},
+
+		// =========================
+		// FRONT-PROXY client
+		// =========================
+		{
+			Name:        "front-proxy-client",
+			Namespace:   ns,
+			SecretName:  "front-proxy-client",
+			CommonName:  "front-proxy-client",
+			IssuerName:  "front-proxy-ca-issuer",
+			Duration:    &tenYears,
+			RenewBefore: &thirtyDays,
+			Usages: []certmanagerv1.KeyUsage{
+				certmanagerv1.UsageDigitalSignature,
+				certmanagerv1.UsageKeyEncipherment,
+				certmanagerv1.UsageClientAuth,
+			},
+		},
+
+		// =========================
+		// Controller-manager / Scheduler client certs
+		// =========================
+		{
+			Name:        "cm-client",
+			Namespace:   ns,
+			SecretName:  "cm-client",
+			CommonName:  "system:kube-controller-manager",
+			IssuerName:  "ca-issuer",
+			Duration:    &tenYears,
+			RenewBefore: &thirtyDays,
+			Usages: []certmanagerv1.KeyUsage{
+				certmanagerv1.UsageDigitalSignature,
+				certmanagerv1.UsageKeyEncipherment,
+				certmanagerv1.UsageClientAuth,
+			},
+		},
+		{
+			Name:        "scheduler-client",
+			Namespace:   ns,
+			SecretName:  "scheduler-client",
+			CommonName:  "system:kube-scheduler",
+			IssuerName:  "ca-issuer",
+			Duration:    &tenYears,
+			RenewBefore: &thirtyDays,
+			Usages: []certmanagerv1.KeyUsage{
+				certmanagerv1.UsageDigitalSignature,
+				certmanagerv1.UsageKeyEncipherment,
+				certmanagerv1.UsageClientAuth,
+			},
+		},
+	}
+
+	for _, cs := range certSpecs {
+		objs = append(objs, BuildCertificate(cs))
+	}
+
+	return objs
+}
+
+func BuildIssuer(spec PKIIssuerSpec) *certmanagerv1.Issuer {
+	issuer := &certmanagerv1.Issuer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      spec.Name,
+			Namespace: spec.Namespace,
+		},
+	}
+
+	switch {
+	case spec.SelfSigned:
+		issuer.Spec = certmanagerv1.IssuerSpec{
 			IssuerConfig: certmanagerv1.IssuerConfig{
 				SelfSigned: &certmanagerv1.SelfSignedIssuer{},
 			},
-		},
-	}
-}
-
-func caCertificate(namespace string) client.Object {
-	return &certmanagerv1.Certificate{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "managed-ca",
-			Namespace: namespace,
-		},
-		Spec: certmanagerv1.CertificateSpec{
-			SecretName: "managed-ca",
-			IsCA:       true,
-			CommonName: "managed-ca",
-			PrivateKey: &certmanagerv1.CertificatePrivateKey{
-				Algorithm: certmanagerv1.RSAKeyAlgorithm,
-				Size:      2048,
-			},
-			IssuerRef: v1.IssuerReference{
-				Name: "selfsigned",
-				Kind: "Issuer",
-			},
-		},
-	}
-}
-
-func caIssuer(namespace string) client.Object {
-	return &certmanagerv1.Issuer{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "ca-issuer",
-			Namespace: namespace,
-		},
-		Spec: certmanagerv1.IssuerSpec{
+		}
+	case spec.CASecret != "":
+		issuer.Spec = certmanagerv1.IssuerSpec{
 			IssuerConfig: certmanagerv1.IssuerConfig{
 				CA: &certmanagerv1.CAIssuer{
-					SecretName: "managed-ca",
+					SecretName: spec.CASecret,
 				},
 			},
+		}
+	}
+
+	return issuer
+}
+
+func BuildCertificate(spec PKICertificateSpec) *certmanagerv1.Certificate {
+	c := &certmanagerv1.Certificate{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      spec.Name,
+			Namespace: spec.Namespace,
 		},
 	}
+
+	certSpec := certmanagerv1.CertificateSpec{
+		SecretName: spec.SecretName,
+		CommonName: spec.CommonName,
+		IsCA:       spec.IsCA,
+		IssuerRef: certmanagermeta.IssuerReference{
+			Name: spec.IssuerName,
+			Kind: "Issuer",
+		},
+		PrivateKey: &certmanagerv1.CertificatePrivateKey{
+			Algorithm: certmanagerv1.RSAKeyAlgorithm,
+			Size:      2048,
+		},
+	}
+
+	if spec.Duration != nil {
+		certSpec.Duration = spec.Duration
+	}
+	if spec.RenewBefore != nil {
+		certSpec.RenewBefore = spec.RenewBefore
+	}
+	if len(spec.Usages) > 0 {
+		certSpec.Usages = spec.Usages
+	}
+	if len(spec.DNSNames) > 0 {
+		certSpec.DNSNames = spec.DNSNames
+	}
+	if len(spec.IPAddresses) > 0 {
+		certSpec.IPAddresses = spec.IPAddresses
+	}
+	if len(spec.Organizations) > 0 {
+		certSpec.Subject = &certmanagerv1.X509Subject{
+			Organizations: spec.Organizations,
+		}
+	}
+
+	c.Spec = certSpec
+	return c
 }
