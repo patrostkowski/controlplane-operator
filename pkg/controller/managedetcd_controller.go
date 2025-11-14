206207cfddb54b53ebc95610041109494aa626a4
Patryk Rostkowski <patrostkowski@gmail.com>
2025-11-15 00:24:50 +0800

    chore: bootstrap etcd creation
    Signed-off-by: Patryk Rostkowski <patrostkowski@gmail.com>

--- pkg/controller/managedetcd_controller.go
+++ pkg/controller/managedetcd_controller.go
@@ -16,14 +16,23 @@ package controller
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-logr/logr"
 	mcpv1alpha1 "github.com/patrostkowski/operator-template/pkg/apis/controlplane.patrostkowski.dev/v1alpha1"
+	"github.com/patrostkowski/operator-template/pkg/controlplane/etcd"
+	"github.com/patrostkowski/operator-template/pkg/controlplane/utils"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	apimeta "k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/tools/record"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
 type ManagedETCDReconciler struct {
@@ -35,14 +44,166 @@ type ManagedETCDReconciler struct {
 
 func (r *ManagedETCDReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := r.Log.WithValues("managedetcd", req.NamespacedName)
-	log.Info("Reconciling managedetcd")
-	log.Info("Finished reconciling managedetcd")
+	log.Info("Reconciling ManagedETCD")
+
+	etcdObj := &mcpv1alpha1.ManagedETCD{}
+	if err := r.Get(ctx, req.NamespacedName, etcdObj); err != nil {
+		if apierrors.IsNotFound(err) {
+			return ctrl.Result{}, nil
+		}
+		return ctrl.Result{}, err
+	}
+
+	resources := etcd.Resources(etcdObj)
+
+	if err := r.ensureResources(ctx, etcdObj, resources, log); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	allReady, err := r.checkResourcesReady(ctx, resources, log)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if err := r.updateETCDReadyCondition(ctx, etcdObj, allReady); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	if !allReady {
+		log.Info("requeueing reconcile for ManagedETCD")
+		return reconcile.Result{RequeueAfter: 10 * time.Second}, nil
+	}
+
+	log.Info("Finished reconciling ManagedETCD")
 	return ctrl.Result{}, nil
+}
+
+func (r *ManagedETCDReconciler) ensureResources(
+	ctx context.Context,
+	etcdObj *mcpv1alpha1.ManagedETCD,
+	resources []client.Object,
+	log logr.Logger,
+) error {
+	for _, desired := range resources {
+		log.Info("Ensuring etcd resource", "kind", desired.GetObjectKind().GroupVersionKind().Kind, "name", desired.GetName())
+
+		err := utils.EnsureCreatedAndOwned(ctx, r.Client, r.Scheme, etcdObj, desired, log, func(obj client.Object) error {
+			switch o := obj.(type) {
+			case *corev1.Service:
+				d := desired.(*corev1.Service)
+				// Preserve clusterIP on updates
+				clusterIP := o.Spec.ClusterIP
+				o.Spec = d.Spec
+				if clusterIP != "" {
+					o.Spec.ClusterIP = clusterIP
+				}
+			case *appsv1.StatefulSet:
+				d := desired.(*appsv1.StatefulSet)
+				// Keep existing status, only spec/labels/annotations etc.
+				o.Spec = d.Spec
+				o.Labels = mergeStringMap(o.Labels, d.Labels)
+				o.Annotations = mergeStringMap(o.Annotations, d.Annotations)
+			}
+			return nil
+		})
+		if err != nil {
+			log.Error(err, "failed to ensure etcd resource", "name", desired.GetName())
+			return err
+		}
+	}
+	return nil
+}
+
+func (r *ManagedETCDReconciler) checkResourcesReady(
+	ctx context.Context,
+	resources []client.Object,
+	log logr.Logger,
+) (bool, error) {
+	allReady := true
+
+	for _, desired := range resources {
+		switch desired.(type) {
+
+		case *appsv1.StatefulSet:
+			key := client.ObjectKey{Namespace: desired.GetNamespace(), Name: desired.GetName()}
+
+			sts := &appsv1.StatefulSet{}
+			if err := r.Get(ctx, key, sts); err != nil {
+				if !apierrors.IsNotFound(err) {
+					log.Error(err, "failed to get StatefulSet", "name", key.Name)
+					return false, err
+				}
+				log.Info("StatefulSet not found yet", "name", key.Name)
+				allReady = false
+				continue
+			}
+
+			desiredReplicas := int32(1)
+			if sts.Spec.Replicas != nil {
+				desiredReplicas = *sts.Spec.Replicas
+			}
+
+			if sts.Status.ReadyReplicas < desiredReplicas {
+				log.Info("StatefulSet not ready", "name", sts.Name, "readyReplicas", sts.Status.ReadyReplicas, "desiredReplicas", desiredReplicas)
+				allReady = false
+			} else {
+				log.Info("StatefulSet ready", "name", sts.Name)
+			}
+		}
+	}
+
+	return allReady, nil
+}
+
+func (r *ManagedETCDReconciler) updateETCDReadyCondition(
+	ctx context.Context,
+	etcdObj *mcpv1alpha1.ManagedETCD,
+	allReady bool,
+) error {
+	var cond metav1.Condition
+
+	if allReady {
+		cond = metav1.Condition{
+			Type:               "Ready",
+			Status:             metav1.ConditionTrue,
+			Reason:             "AllResourcesReady",
+			Message:            "etcd StatefulSet is Ready",
+			ObservedGeneration: etcdObj.Generation,
+		}
+	} else {
+		cond = metav1.Condition{
+			Type:               "Ready",
+			Status:             metav1.ConditionFalse,
+			Reason:             "WaitingForResources",
+			Message:            "Waiting for etcd StatefulSet to become Ready",
+			ObservedGeneration: etcdObj.Generation,
+		}
+	}
+
+	if apimeta.SetStatusCondition(&etcdObj.Status.Conditions, cond) {
+		return r.Status().Update(ctx, etcdObj)
+	}
+	return nil
+}
+
+func mergeStringMap(dst, src map[string]string) map[string]string {
+	if dst == nil && src == nil {
+		return nil
+	}
+	if dst == nil {
+		dst = map[string]string{}
+	}
+	for k, v := range src {
+		dst[k] = v
+	}
+	return dst
 }
 
 func SetupManagedETCDReconciler(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&mcpv1alpha1.ManagedETCD{}).
+		Owns(&appsv1.StatefulSet{}).
+		Owns(&corev1.Service{}).
 		WithOptions(controller.Options{MaxConcurrentReconciles: 1}).
 		Complete(&ManagedETCDReconciler{
 			Client:   mgr.GetClient(),
---
+++ pkg/controlplane/etcd/etcd.go
@@ -0,0 +1,191 @@
+// Copyright 2025 controlplane.patrostkowski.dev
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package etcd
+
+import (
+	mcpv1alpha1 "github.com/patrostkowski/operator-template/pkg/apis/controlplane.patrostkowski.dev/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// Resources returns the Service + StatefulSet required for etcd.
+func Resources(etcdObj *mcpv1alpha1.ManagedETCD) []client.Object {
+	ns := etcdObj.Namespace
+	// For now we hardcode "etcd" to match your cert SANs: etcd-0.etcd.<ns>.svc
+	name := "etcd"
+
+	return []client.Object{
+		buildService(ns, name),
+		buildStatefulSet(ns, name),
+	}
+}
+
+func buildService(namespace, name string) *corev1.Service {
+	labels := map[string]string{
+		"app": "etcd",
+	}
+
+	return &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels:    labels,
+		},
+		Spec: corev1.ServiceSpec{
+			ClusterIP: "None", // headless for StatefulSet
+			Selector:  labels,
+			Ports: []corev1.ServicePort{
+				{
+					Name: "client",
+					Port: 2379,
+				},
+				{
+					Name: "peer",
+					Port: 2380,
+				},
+			},
+		},
+	}
+}
+
+func buildStatefulSet(namespace, name string) *appsv1.StatefulSet {
+	labels := map[string]string{
+		"app": "etcd",
+	}
+
+	replicas := int32(1)
+
+	return &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+			Labels:    labels,
+		},
+		Spec: appsv1.StatefulSetSpec{
+			ServiceName: name,
+			Replicas:    &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:  "etcd",
+							Image: "registry.k8s.io/etcd:3.5.15-0",
+							Command: []string{
+								"etcd",
+							},
+							Args: []string{
+								"--name=etcd-0",
+								"--data-dir=/var/lib/etcd",
+								"--listen-client-urls=https://0.0.0.0:2379",
+								"--advertise-client-urls=https://etcd-0." + name + "." + namespace + ".svc:2379",
+								"--listen-peer-urls=https://0.0.0.0:2380",
+								"--initial-advertise-peer-urls=https://etcd-0." + name + "." + namespace + ".svc:2380",
+								"--initial-cluster=etcd-0=https://etcd-0." + name + "." + namespace + ".svc:2380",
+								"--initial-cluster-state=new",
+								"--client-cert-auth=true",
+								"--peer-client-cert-auth=true",
+								"--trusted-ca-file=/etc/etcd/pki/ca/ca.crt",
+								"--cert-file=/etc/etcd/pki/server/tls.crt",
+								"--key-file=/etc/etcd/pki/server/tls.key",
+								"--peer-trusted-ca-file=/etc/etcd/pki/ca/ca.crt",
+								"--peer-cert-file=/etc/etcd/pki/peer/tls.crt",
+								"--peer-key-file=/etc/etcd/pki/peer/tls.key",
+							},
+							Ports: []corev1.ContainerPort{
+								{Name: "client", ContainerPort: 2379},
+								{Name: "peer", ContainerPort: 2380},
+							},
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "etcd-data",
+									MountPath: "/var/lib/etcd",
+								},
+								{
+									Name:      "etcd-server-tls",
+									MountPath: "/etc/etcd/pki/server",
+									ReadOnly:  true,
+								},
+								{
+									Name:      "etcd-peer-tls",
+									MountPath: "/etc/etcd/pki/peer",
+									ReadOnly:  true,
+								},
+								{
+									Name:      "etcd-ca",
+									MountPath: "/etc/etcd/pki/ca",
+									ReadOnly:  true,
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "etcd-server-tls",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "etcd-server-tls",
+								},
+							},
+						},
+						{
+							Name: "etcd-peer-tls",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "etcd-peer-tls",
+								},
+							},
+						},
+						{
+							Name: "etcd-ca",
+							VolumeSource: corev1.VolumeSource{
+								Secret: &corev1.SecretVolumeSource{
+									SecretName: "etcd-ca",
+								},
+							},
+						},
+					},
+				},
+			},
+			// Use PVC for data; default StorageClass will be used when StorageClassName is nil
+			VolumeClaimTemplates: []corev1.PersistentVolumeClaim{
+				{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "etcd-data",
+					},
+					Spec: corev1.PersistentVolumeClaimSpec{
+						AccessModes: []corev1.PersistentVolumeAccessMode{
+							corev1.ReadWriteOnce,
+						},
+						Resources: corev1.VolumeResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceStorage: resource.MustParse("10Gi"),
+							},
+						},
+						// StorageClassName: nil -> use default StorageClass
+					},
+				},
+			},
+		},
+	}
+}
