859cc9f035ea7fb1b1cd770e34ef8992576d0e47
Patryk Rostkowski <patrostkowski@gmail.com>
2025-12-17 14:46:40 +0100

    fix: adjust apiserver resources layout
    Signed-off-by: Patryk Rostkowski <patrostkowski@gmail.com>

--- pkg/resources/apiserver/apiserver.go
+++ pkg/resources/apiserver/apiserver.go
@@ -15,16 +15,16 @@
 package apiserver
 
 import (
+	"path/filepath"
+
 	mcpv1alpha1 "github.com/patrostkowski/controlplane-operator/pkg/apis/controlplane.patrostkowski.dev/v1alpha1"
+	"github.com/patrostkowski/controlplane-operator/pkg/resources/common"
+	"github.com/patrostkowski/controlplane-operator/pkg/resources/pki"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-)
-
-const (
-	KubeAPIServerSvcName = "kube-apiserver"
 )
 
 // Resources returns Service + Deployment for the kube-apiserver.
@@ -47,14 +47,11 @@ func WorkloadResources(api *mcpv1alpha1.ManagedControlPlane) []client.Object {
 
 func buildService(api *mcpv1alpha1.ManagedControlPlane) *corev1.Service {
 	ns := api.Namespace
-	name := KubeAPIServerSvcName
-	labels := map[string]string{
-		"app": "kube-apiserver",
-	}
+	labels := map[string]string{appLabelKey: appLabelVal}
 
 	return &corev1.Service{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
+			Name:      KubeAPIServerSvcName,
 			Namespace: ns,
 			Labels:    labels,
 		},
@@ -63,8 +60,8 @@ func buildService(api *mcpv1alpha1.ManagedControlPlane) *corev1.Service {
 			Ports: []corev1.ServicePort{
 				{
 					Name:       "https",
-					Port:       6443,
-					TargetPort: intstrFromInt(6443),
+					Port:       securePort,
+					TargetPort: intstrFromInt(securePort),
 				},
 			},
 			Type: corev1.ServiceTypeLoadBalancer,
@@ -74,120 +71,110 @@ func buildService(api *mcpv1alpha1.ManagedControlPlane) *corev1.Service {
 
 func buildDeployment(api *mcpv1alpha1.ManagedControlPlane) *appsv1.Deployment {
 	ns := api.Namespace
-	name := "kube-apiserver"
-	labels := map[string]string{"app": "kube-apiserver"}
+	labels := map[string]string{appLabelKey: appLabelVal}
 	replicas := int32(1)
 
 	version := api.Spec.Version
 
+	// Volume names == mounted directory names under mountRoot
+	caVol := pki.SecretManagedCA
+	apiTLSVol := pki.SecretAPIServerTLS
+	etcdCAVol := pki.SecretEtcdCA
+	etcdClientVol := pki.SecretAPIServerEtcd
+	kubeletClientVol := pki.SecretAPIServerKubelet
+	saVol := pki.SecretSASigner
+	frontProxyCAVol := pki.SecretFrontProxyCA
+	frontProxyClientVol := pki.SecretFrontProxyClient
+
+	// Helper: compute file paths inside mounts
+	certPath := func(vol string) string { return filepath.Join(mountRoot, vol, common.TlsCrt) }
+	keyPath := func(vol string) string { return filepath.Join(mountRoot, vol, common.TlsKey) }
+
 	return &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      name,
+			Name:      apiServerName,
 			Namespace: ns,
 			Labels:    labels,
 		},
 		Spec: appsv1.DeploymentSpec{
 			Replicas: &replicas,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: labels,
-			},
+			Selector: &metav1.LabelSelector{MatchLabels: labels},
 			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: labels,
-				},
+				ObjectMeta: metav1.ObjectMeta{Labels: labels},
 				Spec: corev1.PodSpec{
 					Containers: []corev1.Container{
 						{
 							Name:            "apiserver",
-							Image:           "registry.k8s.io/kube-apiserver" + ":" + version,
+							Image:           "registry.k8s.io/kube-apiserver:" + version,
 							ImagePullPolicy: corev1.PullIfNotPresent,
-							Command: []string{
-								"kube-apiserver",
-							},
+							Command:         []string{"kube-apiserver"},
 							Args: []string{
 								"--advertise-address=" + api.Status.Address,
 								"--bind-address=0.0.0.0",
 								"--secure-port=6443",
 								"--service-cluster-ip-range=" + api.Spec.Networking.ServiceCIDR,
 
+								// etcd
 								"--etcd-servers=https://etcd-0.etcd." + ns + ".svc:2379",
-								"--etcd-cafile=/var/run/k8s/etcd-ca/ca.crt",
-								"--etcd-certfile=/var/run/k8s/etcd-client/tls.crt",
-								"--etcd-keyfile=/var/run/k8s/etcd-client/tls.key",
+								"--etcd-cafile=" + certPath(etcdCAVol),
+								"--etcd-certfile=" + certPath(etcdClientVol),
+								"--etcd-keyfile=" + keyPath(etcdClientVol),
 
-								"--client-ca-file=/var/run/k8s/ca/tls.crt",
-								"--tls-cert-file=/var/run/k8s/apiserver/tls.crt",
-								"--tls-private-key-file=/var/run/k8s/apiserver/tls.key",
+								// serving + client-ca
+								"--client-ca-file=" + certPath(caVol),
+								"--tls-cert-file=" + certPath(apiTLSVol),
+								"--tls-private-key-file=" + keyPath(apiTLSVol),
 
-								"--kubelet-client-certificate=/var/run/k8s/kubelet-client/tls.crt",
-								"--kubelet-client-key=/var/run/k8s/kubelet-client/tls.key",
+								// kubelet client
+								"--kubelet-client-certificate=" + certPath(kubeletClientVol),
+								"--kubelet-client-key=" + keyPath(kubeletClientVol),
 								"--kubelet-preferred-address-types=InternalIP,Hostname,InternalDNS,ExternalIP,ExternalDNS",
 
 								"--authorization-mode=Node,RBAC",
 								"--enable-bootstrap-token-auth=true",
 
+								// service account signing
 								"--service-account-issuer=https://kubernetes.default.svc.cluster.local",
-								"--service-account-key-file=/var/run/k8s/sa/tls.crt",
-								"--service-account-signing-key-file=/var/run/k8s/sa/tls.key",
+								"--service-account-key-file=" + certPath(saVol),
+								"--service-account-signing-key-file=" + keyPath(saVol),
 
 								"--allow-privileged=true",
 
-								"--requestheader-client-ca-file=/var/run/k8s/front-proxy-ca/tls.crt",
-								"--requestheader-allowed-names=front-proxy-client",
+								// front-proxy
+								"--requestheader-client-ca-file=" + certPath(frontProxyCAVol),
+								"--requestheader-allowed-names=" + pki.CNFrontProxyClient,
 								"--requestheader-extra-headers-prefix=X-Remote-Extra-",
 								"--requestheader-group-headers=X-Remote-Group",
 								"--requestheader-username-headers=X-Remote-User",
-								"--proxy-client-cert-file=/var/run/k8s/front-proxy-client/tls.crt",
-								"--proxy-client-key-file=/var/run/k8s/front-proxy-client/tls.key",
+								"--proxy-client-cert-file=" + certPath(frontProxyClientVol),
+								"--proxy-client-key-file=" + keyPath(frontProxyClientVol),
 							},
 							Ports: []corev1.ContainerPort{
-								{Name: "https", ContainerPort: 6443},
+								{Name: "https", ContainerPort: securePort},
 							},
-							LivenessProbe: &corev1.Probe{
-								ProbeHandler: corev1.ProbeHandler{
-									HTTPGet: &corev1.HTTPGetAction{
-										Scheme: corev1.URISchemeHTTPS,
-										Host:   "127.0.0.1",
-										Port:   intstrFromInt(6443),
-										Path:   "/livez",
-									},
-								},
-								InitialDelaySeconds: 10,
-								PeriodSeconds:       10,
-							},
-							ReadinessProbe: &corev1.Probe{
-								ProbeHandler: corev1.ProbeHandler{
-									HTTPGet: &corev1.HTTPGetAction{
-										Scheme: corev1.URISchemeHTTPS,
-										Host:   "127.0.0.1",
-										Port:   intstrFromInt(6443),
-										Path:   "/readyz",
-									},
-								},
-								InitialDelaySeconds: 5,
-								PeriodSeconds:       5,
-							},
+							LivenessProbe:  probeHTTPS(securePort, "/livez", 10, 10),
+							ReadinessProbe: probeHTTPS(securePort, "/readyz", 5, 5),
 							VolumeMounts: []corev1.VolumeMount{
-								{Name: "ca", MountPath: "/var/run/k8s/ca", ReadOnly: true},
-								{Name: "apiserver-tls", MountPath: "/var/run/k8s/apiserver", ReadOnly: true},
-								{Name: "etcd-ca", MountPath: "/var/run/k8s/etcd-ca", ReadOnly: true},
-								{Name: "etcd-client", MountPath: "/var/run/k8s/etcd-client", ReadOnly: true},
-								{Name: "kubelet-client", MountPath: "/var/run/k8s/kubelet-client", ReadOnly: true},
-								{Name: "sa-signer", MountPath: "/var/run/k8s/sa", ReadOnly: true},
-								{Name: "front-proxy-ca", MountPath: "/var/run/k8s/front-proxy-ca", ReadOnly: true},
-								{Name: "front-proxy-client", MountPath: "/var/run/k8s/front-proxy-client", ReadOnly: true},
+								secretMount(caVol),
+								secretMount(apiTLSVol),
+								secretMount(etcdCAVol),
+								secretMount(etcdClientVol),
+								secretMount(kubeletClientVol),
+								secretMount(saVol),
+								secretMount(frontProxyCAVol),
+								secretMount(frontProxyClientVol),
 							},
 						},
 					},
 					Volumes: []corev1.Volume{
-						{Name: "ca", VolumeSource: secretVolume("managed-ca")},
-						{Name: "apiserver-tls", VolumeSource: secretVolume("apiserver-tls")},
-						{Name: "etcd-ca", VolumeSource: secretVolume("etcd-ca")},
-						{Name: "etcd-client", VolumeSource: secretVolume("apiserver-etcd-client")},
-						{Name: "kubelet-client", VolumeSource: secretVolume("apiserver-kubelet-client")},
-						{Name: "sa-signer", VolumeSource: secretVolume("sa-signer")},
-						{Name: "front-proxy-ca", VolumeSource: secretVolume("front-proxy-ca")},
-						{Name: "front-proxy-client", VolumeSource: secretVolume("front-proxy-client")},
+						secretVol(caVol),
+						secretVol(apiTLSVol),
+						secretVol(etcdCAVol),
+						secretVol(etcdClientVol),
+						secretVol(kubeletClientVol),
+						secretVol(saVol),
+						secretVol(frontProxyCAVol),
+						secretVol(frontProxyClientVol),
 					},
 				},
 			},
@@ -195,14 +182,38 @@ func buildDeployment(api *mcpv1alpha1.ManagedControlPlane) *appsv1.Deployment {
 	}
 }
 
+func probeHTTPS(port int32, path string, initialDelay, period int32) *corev1.Probe {
+	return &corev1.Probe{
+		ProbeHandler: corev1.ProbeHandler{
+			HTTPGet: &corev1.HTTPGetAction{
+				Scheme: corev1.URISchemeHTTPS,
+				Host:   "127.0.0.1",
+				Port:   intstrFromInt(port),
+				Path:   path,
+			},
+		},
+		InitialDelaySeconds: initialDelay,
+		PeriodSeconds:       period,
+	}
+}
+
 func intstrFromInt(port int32) intstr.IntOrString {
 	return intstr.IntOrString{Type: intstr.Int, IntVal: port}
 }
 
-func secretVolume(name string) corev1.VolumeSource {
-	return corev1.VolumeSource{
-		Secret: &corev1.SecretVolumeSource{
-			SecretName: name,
+func secretMount(secretName string) corev1.VolumeMount {
+	return corev1.VolumeMount{
+		Name:      secretName,
+		MountPath: filepath.Join(mountRoot, secretName),
+		ReadOnly:  true,
+	}
+}
+
+func secretVol(secretName string) corev1.Volume {
+	return corev1.Volume{
+		Name: secretName,
+		VolumeSource: corev1.VolumeSource{
+			Secret: &corev1.SecretVolumeSource{SecretName: secretName},
 		},
 	}
 }
